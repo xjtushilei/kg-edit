181d1ad20dc30d09e9065665a56fc6e7a98752b2
script <xjtushilei@foxmail.com>
2018-03-16 15:21:26 +0800

    修复-课程目录解析失败

--- src/main/java/com/xjtu/datainput/service/GetChapterListService.java
+++ src/main/java/com/xjtu/datainput/service/GetChapterListService.java
@@ -1,6 +1,7 @@
 package com.xjtu.datainput.service;
 
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
 import com.xjtu.common.Config;
 import com.xjtu.datainput.domain.CatalogListLevel1;
 import com.xjtu.datainput.domain.CatalogListLevel2;
@@ -20,7 +21,7 @@ public class GetChapterListService {
 
 
     public static void main(String[] args) {
-        new GetChapterListService().get("4800FD2B-C9DA-4994-AF88-95DE7C2EF980");
+        new GetChapterListService().get("a7a6e4b7-e5d1-42a4-b7d5-0f7c6a7ff9e5");
     }
 
     public ArrayList<CatalogListLevel1> get(String CourseID) {
@@ -32,9 +33,9 @@ public class GetChapterListService {
         Map<String, Object> map = new HashMap<>();
         map.put("CourseID", CourseID);
         String json = null;
-        String temp= Http.sendGet(Config.MOOC2U_API_GET_ONE_COURSE_INFO + "?CourseID=" + CourseID);
+        String temp = Http.sendGet(Config.MOOC2U_API_GET_ONE_COURSE_INFO + "?CourseID=" + CourseID);
 //            System.out.println(temp);
-        json=temp;
+        json = temp;
 //      json = new String(temp.getBytes( "UTF-8"));
 //        System.out.println(json);
 
@@ -51,34 +52,40 @@ public class GetChapterListService {
             String ParentChapterName = JsonPath.read(string, "$.name").toString();
             String ParentChapterID = JsonPath.read(string, "$.id").toString();
             if (ParentChapterName.equals("期末测试")) continue;
-            //			System.out.println(ParentChapterID+" : "+ParentChapterName);
+//            			System.out.println(ParentChapterID+" ParentChapterID:ParentChapterName "+ParentChapterName);
 
             CatalogListLevel1 level1 = new CatalogListLevel1(ParentChapterID, null, ParentChapterName);
             ArrayList<CatalogListLevel2> level2list = new ArrayList<>();
+            try {
+                List<Map<String, Object>> chapter = JsonPath.read(string, "$.children[*]");
+                for (Map<String, Object> string2 : chapter) {
 
-            List<Map<String, Object>> chapter = JsonPath.read(string, "$.children[*]");
-            for (Map<String, Object> string2 : chapter) {
+                    String ChapterName = JsonPath.read(string2, "$.name").toString();
+                    String ChapterID = JsonPath.read(string2, "$.id").toString();
+//                                				System.out.println(ChapterID+"ChapterID :ChapterName "+ChapterName);
 
-                String ChapterName = JsonPath.read(string2, "$.name").toString();
-                String ChapterID = JsonPath.read(string2, "$.id").toString();
-                //                				System.out.println(ChapterID+" : "+ChapterName);
+                    ArrayList<CatalogListLevel3> level3list = new ArrayList<>();
+                    try {
+                        List<Map<String, Object>> ChildrenChapter = JsonPath.read(string2, "$.children[*]");
+                        for (Map<String, Object> string3 : ChildrenChapter) {
+                            String ChildrenChapterName = JsonPath.read(string3, "$.name").toString();
+                            String ChildrenChapterID = JsonPath.read(string3, "$.id").toString();
+//                        System.out.println(ChildrenChapterID+"ChildrenChapterID :ChildrenChapterName "+ChildrenChapterName);
+                            if (isChapter(ChildrenChapterName)) {
+                                level3list.add(new CatalogListLevel3(ChildrenChapterID, ChildrenChapterName));
+                            }
+                        }
 
-                ArrayList<CatalogListLevel3> level3list = new ArrayList<>();
-                List<Map<String, Object>> ChildrenChapter = JsonPath.read(string2, "$.children[*]");
-                for (Map<String, Object> string3 : ChildrenChapter) {
-                    String ChildrenChapterName = JsonPath.read(string3, "$.name").toString();
-                    String ChildrenChapterID = JsonPath.read(string3, "$.id").toString();
-                    //					System.out.println(ChildrenChapterID+" : "+ChildrenChapterName);
-                    if (isChapter(ChildrenChapterName)) {
-                        level3list.add(new CatalogListLevel3(ChildrenChapterID, ChildrenChapterName));
+                        if (isChapter(ChapterName)) {
+                            level2list.add(new CatalogListLevel2(level3list, ChapterID, ChapterName));
+                        }
+                    } catch (PathNotFoundException e) {
+                        // json中不存在这个字段，自动跳过。有些字段下面没有children(2018年3月16日15:03:08发现的)
                     }
+
                 }
-
-                if (isChapter(ChapterName)) {
-                    level2list.add(new CatalogListLevel2(level3list, ChapterID, ChapterName));
-                }
-
-
+            } catch (PathNotFoundException e) {
+                // json中不存在这个字段，自动跳过。有些字段下面没有children(2018年3月16日15:03:08发现的)
             }
             level1.setChapterList(level2list);
             result.add(level1);
@@ -105,7 +112,6 @@ public class GetChapterListService {
             return true;
         return false;
     }
-
 
 
 }
